0eb1deedb20a0848394633e45eafd1cd2fb7a482
adr1an-n9 <123412234+adr1an-n9@users.noreply.github.com>
2024-03-18 19:20:45 +0200

    dayofweek-number.cpp

--- cpp-base/if else/dayofweek-number.cpp
+++ cpp-base/if else/dayofweek-number.cpp
@@ -7,30 +7,8 @@ int main() {
 	
     cout << "Enter number n: ";
     cin >> n;
-    
-    //this version of solving this type of problem is not good
-    /* 
-    if (n == 1) {
-        cout << "1 = Monday";
-    } else if (n == 2) {
-        cout << "2 = Tuesday";
-    } else if (n == 3) {
-        cout << "3 = Wednesday";
-    } else if (n == 4){
-        cout << "4 = Thursday";
-    } else if (n == 5){
-        cout << "5 = Friday";
-    } else if (n == 6){
-        cout << "6 = Saturday";
-    } else if (n == 7){
-        cout << "7 = Sunday";    
-    } else {
-    	cout << "there are only 7 days in weeks, not " << n;
-    }
-	*/
-	
-    if ( n >= 1 && n <= 7) {
-    	switch (n) {
+    	
+      	switch (n) {
     	  case 1:
     	    cout << "Monday";
     	    break;
@@ -52,9 +30,10 @@ int main() {
     	  case 7:
     	    cout << "Sunday";
     	    break;
+	  default:
+	    cout << "There are only 7 days in a week, not " << n;
+	    break;
     	}
-      } else {
-         cout << "There are only 7 days in a week, not " << n;
-     }
+	
   return 0;
 }
